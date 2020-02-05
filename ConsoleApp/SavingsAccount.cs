316a106402933fc0eaa782204281545f263773fc
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-02-05 12:04:18 +0100

    creation authorize credit methods

--- ConsoleApp/SavingsAccount.cs
+++ ConsoleApp/SavingsAccount.cs
@@ -6,7 +6,7 @@ namespace Project2
 {
     class SavingsAccount : Account
     {
-        public double Ceiling { get; set; }
+        public decimal Ceiling { get; set; } = 1000;
         public double SavingsRate { get; set; }
 
         public SavingsAccount()
@@ -24,6 +24,24 @@ namespace Project2
         {
             IdCustomer = idClient;
             Amount = 0;
+        }
+
+        public override bool IsAuthorizeCustomerToCredit()
+        {
+            if(DBQuery.IsCurrentCustomerAuthorizedOnAccount(this) || (IdCustomer == Program.currentCustomer.IdCustomer))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override bool CanBeCredited(decimal amountToTransfer)
+        {            
+            if ((amountToTransfer + Amount) < Ceiling && IsAuthorizeCustomerToCredit())
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
