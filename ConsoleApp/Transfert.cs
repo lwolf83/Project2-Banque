4ab26f9501ced84cbefd6923ab9439feadf07850
Laurent Wolf <laurent.wolf1983@gmail.coml>
2020-02-19 11:04:21 +0100

    add transfert classé

---
+++ ConsoleApp/Transfert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2
+{
+    class Transfert
+    {
+        public Account Origin { get; set; }
+        public Account Destination { get; set; }
+        public DateTime TransfertDate { get; set; }
+        public Decimal Amount { get; set; }
+        public bool IsDone { get; set; }
+        public Int16 IdTransaction { get; set; }
+    }
+}
