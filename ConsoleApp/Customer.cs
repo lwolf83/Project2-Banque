482a454022f1f8e6c16bf3fc60611c4fdc6e429e
Jean <jean.gus.barthelemy@gmail.com>
2020-02-20 15:49:35 +0100

    issue solved : specials charaters added, isComplexPassword moved in IO

--- ConsoleApp/Customer.cs
+++ ConsoleApp/Customer.cs
@@ -48,75 +48,7 @@ namespace Project2
             }
         }
 
-        public static bool IsComplexPassword(string password)
-        {
-            bool hasUpperletter = HasUpperletter(password);
-            bool hasLowerletter = HasLowerletter(password);
-            bool hasEnoughLetters = HasEnoughLetters(password);
-            bool hasNumber = HasNumber(password);
-            bool hasSpecialCharacter = HasSpecialCharacter(password);
-            bool result = hasUpperletter && hasLowerletter && hasEnoughLetters && hasNumber && hasSpecialCharacter;
-            return result;
-        }
-
-        public static bool HasUpperletter(string password)
-        {
-            foreach(char letter in password)
-            {
-                if (char.IsUpper(letter))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static bool HasLowerletter(string password)
-        {
-            foreach (char letter in password)
-            {
-                if (char.IsLower(letter))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static bool HasEnoughLetters(string password)
-        {
-            if(password.Length >= 8)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public static bool HasNumber(string password)
-        {
-            foreach(char character in password)
-            {
-                if(char.IsDigit(character))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static bool HasSpecialCharacter(string password)
-        {
-            string specialCharacter = "/#+=-*@%&_.;,!?()[]{}<>";
-            foreach(char character in password)
-            {
-                if(specialCharacter.Contains(character))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
+      
         public bool IsCustomerExisting(string login)
         { // vérifie dans la base de données si le client existe en fonction de son login
             Customer existingCustomer = DBQuery.getCustomerFromDbWhereLogin(login);
