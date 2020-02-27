f7ec5f5a2d160c5c9daf3e77ab817c914f89452e
lwolf83 <58726454+lwolf83@users.noreply.github.com>
2020-02-27 11:08:06 +0100

    Update query.sql

--- ConsoleApp/query.sql
+++ ConsoleApp/query.sql
@@ -118,9 +118,9 @@ INSERT INTO [AccountAuthorizedCustomers] (idAccount, idCustomer) VALUES (5, 1)
 INSERT INTO [AccountAuthorizedCustomers] (idAccount, idCustomer) VALUES (5, 2)
 INSERT INTO [AccountAuthorizedCustomers] (idAccount, idCustomer) VALUES (5, 3)
 
-INSERT INTO [Transaction] (idOriginAccount,idDestinationAccount,amount,transactionType,transactionDate,beginDate,endDate,periodicity) VALUES (1, 2, 10, 'deffered','2020-02-22','','','')
-INSERT INTO [Transaction] (idOriginAccount,idDestinationAccount,amount,transactionType,transactionDate,beginDate,endDate,periodicity) VALUES (1, 2, 10, 'deffered','2020-02-24','','','')
-INSERT INTO [Transaction] (idOriginAccount,idDestinationAccount,amount,transactionType,transactionDate,beginDate,endDate,periodicity) VALUES (1, 2, 10, 'deffered','2020-02-26','','','')
+INSERT INTO [Transaction] (idOriginAccount,idDestinationAccount,amount,transactionType,transactionDate,beginDate,endDate,periodicity) VALUES (1, 2, 10, 'DeferredTransaction','2020-02-22','','','')
+INSERT INTO [Transaction] (idOriginAccount,idDestinationAccount,amount,transactionType,transactionDate,beginDate,endDate,periodicity) VALUES (1, 2, 10, 'DeferredTransaction','2020-02-24','','','')
+INSERT INTO [Transaction] (idOriginAccount,idDestinationAccount,amount,transactionType,transactionDate,beginDate,endDate,periodicity) VALUES (1, 2, 10, 'DeferredTransaction','2020-02-26','','','')
 
 INSERT INTO [Transfert] (idOriginAccount,idDestinationAccount,amount,transferDate, isDone, idTransaction) VALUES (1, 2, 10, '2020-02-22', 0, 1)
 INSERT INTO [Transfert] (idOriginAccount,idDestinationAccount,amount,transferDate, isDone, idTransaction) VALUES (1, 2, 10, '2020-02-24', 0, 2)
