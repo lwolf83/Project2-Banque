87a2139619ee4201d09dc44a0bae3031767482e0
Laurent Wolf <laurent.wolf1983@gmail.coml>
2020-02-27 17:23:56 +0100

    Initialisation TransferExecutor

---
+++ JELListener/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JELListener
+{
+    class Transaction
+    {
+        public int Id { get; set; }
+        public int DestinaryAccount { get; set; }
+        public int OriginAccount { get; set; }
+        public IEnumerable<Transfer> Transfers { get; set; }
+    }
+}
